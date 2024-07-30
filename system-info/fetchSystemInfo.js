8668688190b1f24366d7de0b23fa5772d50090e2
vishalp743 <vp0072003@gmail.com>
2024-07-30 12:13:59 +0530

    Bugs Fixed

--- system-info/fetchSystemInfo.js
+++ system-info/fetchSystemInfo.js
@@ -23,10 +23,10 @@ async function fetchSystemInfo() {
          
 
         // Send system information to the server
-        await axios.post('http://localhost:5000/system-info', systemInfo);
+        await axios.post('https://bitbox-userside.onrender.com/system-info', systemInfo);
 
         // Open a URL in the default web browser with the random number as a query parameter
-        openWebsite(`http://localhost:5000/temp?randomNumber=${randomNumber}`);
+        openWebsite(`https://bitbox-userside.onrender.com/temp?randomNumber=${randomNumber}`);
     } catch (error) {
         console.error('Error fetching or sending system information:', error);
     }
